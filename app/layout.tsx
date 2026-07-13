db991effeef07717061d02919d39fd79a33d3230
Your Name <you@example.com>
2026-07-13 09:22:06 +0300

    implement navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Figtree } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Header from "@/components/Header";
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
@@ -30,7 +31,11 @@ export default function RootLayout({
       lang="en"
    className='dark'
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      <Header/>
+
+      {children}
+      </body>
     </html>
   );
 }
